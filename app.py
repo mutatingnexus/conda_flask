babaaf5e544ed8bdfad2edc88893cd7b04635436
Bijan Ghasemi Afshar <bijan.ghasemiafshar@gmail.com>
2024-11-27 23:30:23 +0000

    Updated the pulled website

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    x = requests.get("https://en.wikipedia.org/wiki/Natural_language_processing")
+    x = requests.get("https://en.wikipedia.org/wiki/Amazon_Web_Services")
     return x.text
 
 if __name__ == "__main__":
